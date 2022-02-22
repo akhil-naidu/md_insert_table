9180d8f02298713da38fb4b8d436c15762757fe6
ankurs1ngh <ankur.singh@edumatica.io>
2022-02-22 16:50:23 +0530

    init: console error removed

--- static/js/index.js
+++ static/js/index.js
@@ -4,11 +4,13 @@ exports.eejsBlock_timesliderScripts = function (hook_name, args, cb) {
     require('md_mudoc-lite/node/eejs/').require(
       'md_insert_table/templates/datatablesScriptsTimeslider.ejs',
     );
+    return cb();
 };
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
   args.content =
     args.content +
     require('md_mudoc-lite/node/eejs/').require('md_insert_table/templates/datatablesScripts.ejs');
+    return cb();
 };
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
   args.content =
@@ -16,9 +18,11 @@ exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
     require('md_mudoc-lite/node/eejs/').require(
       'md_insert_table/templates/datatablesEditbarButtons.ejs',
     );
+    return cb();
 };
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content =
     require('md_mudoc-lite/node/eejs/').require('md_insert_table/templates/styles.ejs') +
     args.content;
+    return cb();
 };
